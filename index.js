6da85d0d71d6bb4833851eff3b5c12b085c1c391
Richard Littauer <richard.littauer@gmail.com>
2016-12-05 14:28:26 -0500

    Print time until next call

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const botName = 'sprint-helper'
 
 const valid = require('./validate')
 
+const PublicGcal = require('public-gcal')
+const API_key = process.env.IPFS_CALENDAR_API
+const calendarID = 'ipfs.io_eal36ugu5e75s207gfjcu0ae84@group.calendar.google.com'
+
+const _ = require('lodash')
+const moment = require('moment')
+
+var gcal = new PublicGcal({API_key: API_key, calendarId: calendarID})
+
 const client = new irc.Client('irc.freenode.net', botName, {
     channels: [channel],
     port: 6667
@@ -17,7 +26,30 @@ client.addListener('message', function (from, to, message) {
 
   if (to === channel) {
     message = valid.validateMessage(message, botName)
-    if (message && valid.checkAllArgs(message)) {
+
+    if (message && message.type === 'next') {
+      gcal.getEvents({timeMin: moment(new Date()).toISOString(), timeMax: moment(new Date()).add(1, "week").toISOString()}, function (error, result) {
+        if (error) { return console.log(error) }
+
+        var todaysEvents = _.filter(result, function(event) {
+          return moment(event.start.dateTime).isSame(new Date(), "day")
+        })
+
+        // TODO If event is going on right now, note that if asked
+        var currentEvent = _.filter(todaysEvents, function(event) {
+          return moment(event.start.dateTime).isBefore(moment(new Date()))
+        })
+
+        // If there is an event in the future, note when that is
+        if (currentEvent.length !== 0) {
+          client.say(channel, [
+`The next event is "${todaysEvents[1].summary}", ${moment(todaysEvents[1].start.dateTime).fromNow()}.`])
+        }
+      })
+
+    }
+
+    if (message && message.type === 'template' && valid.checkAllArgs(message)) {
       var header = `========================= IPFS Sprint: ${message.topic} =========================`
       client.say(channel, `
 ${header}
@@ -27,7 +59,7 @@ Notes: ${message.notes}
 Join Call: ${message.zoom}
 Watch Stream: ${message.stream}
 ${Array(stringLength(header) - 3).join('=')}`)
-    } else if (message) {
+    } else if (message && message.type === 'error') {
       var usageMsg = `Correct usage: ${botName}: <topic name> <sprint issue> <notes> <zoom> <stream url or message>`
       var feedback = `Feedback: https://github.com/RichardLitt/ipfs-sprint-helper`
 
